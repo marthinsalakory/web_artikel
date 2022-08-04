bfa231608b093d172542f1dfcba33f74f74f2a3b
marthinsalakory <marthinsalakory11@gmail.com>
2022-08-04 22:36:23 +0900

    mengedit href logout

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Admin <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                            <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                         </li>
                     </ul>
                 </li>
